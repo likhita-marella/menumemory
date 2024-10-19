9ec51648e44112dcb3e894c4b2902c37c1fa0921
Selvaganesh Arunmozhi <thisisselva18@gmail.com>
2024-10-19 06:06:43 +0530

    Problem solved successfully

--- lib/widgets/visit_card.dart
+++ lib/widgets/visit_card.dart
@@ -11,65 +11,125 @@ class VisitCard extends StatefulWidget {
 }
 
 class _VisitCardState extends State<VisitCard> {
-
   bool isMinimized = false;
   bool seeAll = false;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: widget.visit==null ? CircularProgressIndicator() : Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  textBaseline: TextBaseline.ideographic,
-                  children: [
-                    Text(widget.visit!.restaurant_info.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-                    SizedBox(width: 10,),
-                    Text(DateFormat("dd MMM yyyy  K:ma").format(widget.visit.datetime),
-                      )
-                  ],
-                ),
-                IconButton(onPressed: () {
-                  setState(() {
-                    isMinimized = !isMinimized;
-                  });
-                }, icon: Icon(isMinimized ? Icons.arrow_drop_up_sharp : Icons.arrow_drop_down_sharp))
-              ],
+      child: widget.visit == null
+          ? CircularProgressIndicator()
+          : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        textBaseline: TextBaseline.ideographic,
+                        children: [
+                          Text(
+                            widget.visit!.restaurant_info.name,
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            DateFormat("dd MMM yyyy  K:ma")
+                                .format(widget.visit.datetime),
+                          )
+                        ],
+                      ),
+                      IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isMinimized = !isMinimized;
+                            });
+                          },
+                          icon: Icon(isMinimized
+                              ? Icons.arrow_drop_up_sharp
+                              : Icons.arrow_drop_down_sharp))
+                    ],
+                  ),
+                  if (!isMinimized)
+                    ...widget.visit!.order
+                        .sublist(0, seeAll ? widget.visit!.order.length : 2)
+                        .map((order) => Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      order.dish.name,
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                    SizedBox(width: 10),
+                                    Center(
+                                      child: Container(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 8, vertical: 4),
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(20)),
+                                          border: Border.all(
+                                            color: Colors.black,
+                                            width:
+                                                0.5, // Adjust the width for a thicker or thinner border
+                                          ),
+                                        ),
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Text(
+                                              "${order.rating}",
+                                              style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                            SizedBox(width: 4),
+                                            Text(
+                                              "⭐",
+                                              style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Text(
+                                  '"${order.review_text!}"',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                                SizedBox(height: 5),
+                                Divider(thickness: 1, color: Colors.grey)
+                              ],
+                            )),
+                  if (!isMinimized)
+                    Center(
+                        child: TextButton(
+                            onPressed: () {
+                              setState(() {
+                                seeAll = !seeAll;
+                              });
+                            },
+                            child: Text(seeAll ? "See less" : "See more")))
+                ],
+              ),
             ),
-            if (!isMinimized)
-            ...widget.visit!.order.sublist(0, seeAll ? widget.visit!.order.length : 2).map((order) => Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 5),
-                Row(
-                  children: [
-                    Text(order.dish.name, style: TextStyle(fontSize: 20),),
-                    SizedBox(width: 10),
-                    Text("${order.rating}/5")
-                  ],
-                ),
-                Text('"${order.review_text!}"', style: TextStyle(fontSize: 16),),
-                SizedBox(height: 5),
-                Divider(thickness: 1,color: Colors.grey)
-              ],
-            )),
-            if (!isMinimized)
-            Center(child: TextButton(onPressed: (){
-              setState(() {
-                seeAll = !seeAll;
-              });
-            }, child: Text(seeAll ? "See less" : "See more")))
-          ],
-        ),
-      ),
     );
   }
 }
